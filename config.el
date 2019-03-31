85361105b83cd5272ba67888672f19d2e9c05ab8
Jing Yen Loh <lohjingyen@gmail.com>
2019-03-31 17:47:19 +0800

    Initial commit
    Fresh new config, since previous one got wiped

---
+++ config.el
@@ -0,0 +1,26 @@
+;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+
+;; About me
+(setq user-full-name "Jing Yen Loh"
+      user-mail-address "lohjingyen@gmail.com")
+
+;; Some basic settings
+(setq doom-localleader-key ","
+      +default-repeat-keys (cons ";" "'")
+
+      display-line-numbers-type 'relative
+      evil-escape-key-sequence "fd")
+
+;; Start Emacs maximized
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; Org
+(setq org-directory "~/Nextcloud/Documents/org/"
+      +org-capture-todo-file "Inbox.org"
+
+      org-refile-targets '(("Next.org" :level . 0)
+                           ("Someday.org" :level . 0)
+                           ("Projects.org" :maxlevel . 9)))
+
