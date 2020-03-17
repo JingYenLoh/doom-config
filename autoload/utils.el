dd909cf1f490bc0255f7da3819d102cb8ca1354d
Jing Yen Loh <lohjingyen@gmail.com>
2020-03-18 01:01:32 +0800

    Some functions

---
+++ autoload/utils.el
@@ -0,0 +1,46 @@
+;;; ~/.doom.d/autoloads/utils.el -*- lexical-binding: t; -*-
+
+;;;###autoload
+(defun nero/open-modules ()
+  "Opens a dired buffer in my Nextcloud Modules folder."
+  (interactive)
+  (dired "~/Nextcloud/Documents/NUS/Modules"))
+
+;;;###autoload
+(defun nero/open-books ()
+  "Opens a dired buffer in my Nextcloud Books folder."
+  (interactive)
+  (dired "~/Nextcloud/Documents/Books"))
+
+;;;###autoload
+(defun nero/open-documents ()
+  "Opens a dired buffer in my Nextcloud Documents folder."
+  (interactive)
+  (dired "~/Nextcloud/Documents"))
+
+;;;###autoload
+(defun nero/browse-modules ()
+  "Browse the files in my Nextcloud Modules folder."
+  (interactive)
+  (doom-project-browse (expand-file-name "~/Nextcloud/Documents/NUS/Modules")))
+
+(defun nero/insert-word-count-region (start end &optional ARG)
+  "Inserts the word count of a region below it in the format of (%d words)."
+  (interactive (if current-prefix-arg
+       (list nil nil current-prefix-arg)
+     (list (region-beginning) (region-end) nil)))
+  (let ((word-count (count-words start end)))
+    (progn
+      (deactivate-mark)
+      (goto-char end)
+      (move-end-of-line nil)
+      (open-line 2)
+      (forward-line 2)
+      (evil-insert-state)
+      (insert (format "(%d words)" word-count))
+      (evil-normal-state))))
+
+
+;; Foo bar baz
+
+;; chicken wings
