33f48756ca28f4e70bae414638563450d18573a1
Jing Yen Loh <lohjingyen@gmail.com>
2020-03-18 00:59:56 +0800

    Commit package changes too

--- packages.el
+++ packages.el
@@ -5,7 +5,24 @@
 ;; (package! some-package)
 ;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
-(package! org-caldav :recipe
-  (:host github :repo "knatsakis/org-caldav"))
 
-(package! bison-mode)
+(package! deft :recipe
+  (:host github :repo "jingyenloh/deft")
+  :pin "b7d87f176a409d7d82dc8b713c329b3fc169283c")
+
+(package! mixed-pitch :recipe
+  (:host gitlab :repo "lohjingyen/mixed-pitch")
+  :pin "0128a151d8dd2235f1c6f3bf53c9e28777440008")
+
+(package! crux)
+
+;; (package! org-super-agenda)
+
+(package! org-noter)
+(package! org-pdftools :recipe
+  (:host github :repo "fuxialexander/org-pdftools"))
+
+(package! mips-mode)
+
+(package! org-roam :recipe
+  (:host github :repo "jethrokuan/org-roam"))
